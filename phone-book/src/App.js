837cd2288620877fe4573e6c9535bb010ddbe8bc
moran991231 <moran991231@naver.com>
2021-11-03 21:54:02 +0900

    7

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm'
+import PhoneInfoList from './components/PhoneInfoList';
 
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 1,
+        name: 'hong gil dong',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: 'kim min june',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
   }
   render() {
+    const { information } = this.state;
     return (
       <div> <PhoneForm
         onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information} />
       </div>
     )
   }
